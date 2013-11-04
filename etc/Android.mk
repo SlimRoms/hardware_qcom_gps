88779075d08c532041361e9876e913f75e09b754
Nicholas Flintham <nick@flinny.org>
2013-11-04 23:47:26 +0000

    gps : Add TARGET_PROVICES_GPS_LOC_API

--- etc/Android.mk
+++ etc/Android.mk
@@ -1,4 +1,4 @@
-
+ifneq ($(TARGET_PROVIDES_GPS_LOC_API),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -9,4 +9,4 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
 LOCAL_SRC_FILES := gps.conf
 
 include $(BUILD_PREBUILT)
-
+endif#TARGET_PROVIDES_GPS_LOC_API
